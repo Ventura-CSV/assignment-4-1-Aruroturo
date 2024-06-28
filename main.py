2b1d8a9b349c784b8a3e1291b7b7774cdc451f43
Aruroturo <arturo.alfaro3@my.vcccd.edu>
2024-06-28 10:30:35 -0700

    try 3

--- main.py
+++ main.py
@@ -3,6 +3,25 @@ def main():
     while True:
         start = input('Enter the starting letter: ')
         end = input('Enter the starting letter: ')
+        if len(start) == 1 and start.isalpha():
+            start_ord = ord(start)
+        else:
+            print("Error use letters between A and Z")
+            continue
+        if len(end) == 1 and end.isalpha():
+            end_ord = ord(end)
+        else:
+            print("Error use letters between A and Z")
+            continue
+        if start_ord > end_ord:
+            print("Starting letter must be before end letter")
+        else:
+            break
+    for char in range(ord(start), ord(end) +1):
+            result.append(chr(char))
+    print("".join(result))
+            
+        
 
     """
     ########################################
